52054d53140a95aad48bf2a15e6c607b268f22fc
juci11 <160618137+juci11@users.noreply.github.com>
2024-06-30 21:04:13 -0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,1107 @@
+let questions = [
+
+
+
+    {
+
+
+
+        numb: 1,
+
+        question: "Coloque o acento na palavra correta: Palavra: 'bebe'",
+
+        answer: "bebê",
+
+        options: [
+
+            "bebé",
+
+            "bebê",
+
+            "bêbe",
+
+            "bebè"
+
+        ]
+
+    },
+
+    {
+
+        numb: 2,
+
+        question: "Escreva a palavra corretamente acentuada: Palavra: 'pais'",
+
+        answer: "país",
+
+        options: [
+
+            "páis",
+
+            "país",
+
+            "pãis",
+
+            "pais"
+
+        ]
+
+    },
+
+    {
+
+        numb: 3,
+
+        question: "Identifique a acentuação: Palavra: 'maquina'",
+
+        answer: "máquina",
+
+        options: [
+
+            "máquína",
+
+            "máquiná",
+
+            "máquina",
+
+            "maquína"
+
+        ]
+
+    },
+
+    {
+
+        numb: 4,
+
+        question: "Complete a palavra com o acento correto: Palavra: 'acoes'",
+
+        answer: "ações",
+
+        options: [
+
+            "acoés",
+
+            "açõés",
+
+            "ações",
+
+            "açoes"
+
+        ]
+
+    },
+
+    {
+
+        numb: 5,
+
+        question: "Reescreva a palavra com a acentuação adequada: Palavra: 'relogio'",
+
+        answer: "relógio",
+
+        options: [
+
+            "relógio",
+
+            "relógío",
+
+            "rélogio",
+
+            "relógiô"
+
+        ]
+
+    },
+
+    {
+
+        numb: 6,
+
+        question: "Marque o local onde deve ser colocado o acento: Palavra: 'exercito'",
+
+        answer: "exército",
+
+        options: [
+
+            "éxercito",
+
+            "exercíto",
+
+            "exércîto",
+
+            "exército"
+
+        ]
+
+    },
+
+    {
+
+        numb: 7,
+
+        question: "Escreva a palavra acentuada corretamente: Palavra: 'mapa'",
+
+        answer: "mapá",
+
+        options: [
+
+            "mápá",
+
+            "mapá",
+
+            "mápa",
+
+            "mapeá"
+
+        ]
+
+    },
+
+    {
+
+        numb: 8,
+
+        question: "Indique a posição correta do acento: Palavra: 'rapida'",
+
+        answer: "rápida",
+
+        options: [
+
+            "rápída",
+
+            "rápída",
+
+            "rápida",
+
+            "rápiâda"
+
+        ]
+
+    },
+
+    {
+
+        numb: 9,
+
+        question: "Marque o local onde deve ser colocado o acento: Palavra: 'agua'",
+
+        answer: "água",
+
+        options: [
+
+            "agua",
+
+            "água",
+
+            "águá",
+
+            "agúa"
+
+        ]
+
+    },
+
+    {
+
+        numb: 10,
+
+        question: "Marque o local onde deve ser colocado o acento: Palavra: 'lingua'",
+
+        answer: "língua",
+
+        options: [
+
+            "língua",
+
+            "lingua",
+
+            "linguá",
+
+            "líguá"
+
+        ]
+
+    },
+
+    {
+
+        numb: 11,
+
+        question: "Marque o local onde deve ser colocado o acento: Palavra: 'heroi'",
+
+        answer: "herói",
+
+        options: [
+
+            "héroi",
+
+            "herói",
+
+            "heroí",
+
+            "heroi"
+
+        ]
+
+    },
+
+    {
+
+        numb: 12,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'musica':",
+
+        answer: "música",
+
+        options: [
+
+            "musica",
+
+            "múzica",
+
+            "mùsica",
+
+            "música"
+
+        ]
+
+    },
+
+    {
+
+        numb: 13,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'jiboia':",
+
+        answer: "jibóia",
+
+        options: [
+
+            "jiboia",
+
+            "jíboia",
+
+            "jibóia",
+
+            "jiboía"
+
+        ]
+
+    },
+
+    {
+
+        numb: 14,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'medico':",
+
+        answer: "médico",
+
+        options: [
+
+            "medico",
+
+            "médíco",
+
+            "mèdico",
+
+            "médico"
+
+        ]
+
+    },
+
+    {
+
+        numb: 15,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'sabio':",
+
+        answer: "sábio",
+
+        options: [
+
+            "sabio",
+
+            "sábio",
+
+            "sàbio",
+
+            "sábío"
+
+        ]
+
+    },
+
+    {
+
+        numb: 16,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'torax':",
+
+        answer: "tórax",
+
+        options: [
+
+            "torax",
+
+            "tôrax",
+
+            "toráx",
+
+            "tórax"
+
+        ]
+
+    },
+
+    {
+
+        numb: 17,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'obvio':",
+
+        answer: "óbvio",
+
+        options: [
+
+            "obvio",
+
+            "òbvio",
+
+            "óbvío",
+
+            "óbvio"
+
+        ]
+
+    },
+
+    {
+
+        numb: 18,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'onibus':",
+
+        answer: "ônibus",
+
+        options: [
+
+            "onibus",
+
+            "ôníbus",
+
+            "ônibús",
+
+            "ônibus"
+
+        ]
+
+    },
+
+    {
+
+        numb: 19,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'animo':",
+
+        answer: "ânimo",
+
+        options: [
+
+            "animo",
+
+            "ânímo",
+
+            "ânîmo",
+
+            "ânimo"
+
+        ]
+
+    },
+
+    {
+
+        numb: 20,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'comodo':",
+
+        answer: "cômodo",
+
+        options: [
+
+            "comodo",
+
+            "cômodô",
+
+            "cômodo",
+
+            "cômodo"
+
+        ]
+
+    },
+
+    {
+
+        numb: 21,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'matematica':",
+
+        answer: "matemática",
+
+        options: [
+
+            "matemática",
+
+            "matemátíca",
+
+            "matématica",
+
+            "matemátíca"
+
+        ]
+
+    },
+
+    {
+
+        numb: 22,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'armario':",
+
+        answer: "armário",
+
+        options: [
+
+            "armário",
+
+            "armárío",
+
+            "ármario",
+
+            "ármárîo"
+
+        ]
+
+    },
+
+    {
+
+        numb: 23,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'docil':",
+
+        answer: "dócil",
+
+        options: [
+
+            "docíl",
+
+            "dócil",
+
+            "dóçil",
+
+            "dòçil"
+
+        ]
+
+    },
+
+    {
+
+        numb: 24,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'acucar':",
+
+        answer: "açúcar",
+
+        options: [
+
+            "açucar",
+
+            "açúcar",
+
+            "açúcár",
+
+            "acúcar"
+
+        ]
+
+    },
+
+    {
+
+        numb: 25,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'analogico':",
+
+        answer: "analógico",
+
+        options: [
+
+            "analogico",
+
+            "analógico",
+
+            "analógíco",
+
+            "analógiço"
+
+        ]
+
+    },
+
+    {
+
+        numb: 26,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'parabens':",
+
+        answer: "parabéns",
+
+        options: [
+
+            "parabéns",
+
+            "parabens",
+
+            "parábens",
+
+            "parâbens"
+
+        ]
+
+    },
+
+    {
+
+        numb: 27,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'celula':",
+
+        answer: "célula",
+
+        options: [
+
+            "célula",
+
+            "celúla",
+
+            "cèlula",
+
+            "cêlula"
+
+        ]
+
+    },
+
+    {
+
+        numb: 28,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'grafico':",
+
+        answer: "gráfico",
+
+        options: [
+
+            "gráfico",
+
+            "gráficô",
+
+            "gràfico",
+
+            "gráphico"
+
+        ]
+
+    },
+
+    {
+
+        numb: 29,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'acido':",
+
+        answer: "ácido",
+
+        options: [
+
+            "acido",
+
+            "ácído",
+
+            "áçido",
+
+            "ácìdo"
+
+        ]
+
+    },
+
+    {
+
+        numb: 30,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'cafe':",
+
+        answer: "café",
+
+        options: [
+
+            "cafe",
+
+            "cáfe",
+
+            "cafè",
+
+            "cafê"
+
+        ]
+
+    },
+
+    {
+
+        numb: 31,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'estrategia':",
+
+        answer: "estratégia",
+
+        options: [
+
+            "estratégia",
+
+            "estrátegia",
+
+            "estratègia",
+
+            "estrátegìa"
+
+        ]
+
+    },
+
+    {
+
+        numb: 32,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'lampada':",
+
+        answer: "lâmpada",
+
+        options: [
+
+            "lâmpada",
+
+            "lampada",
+
+            "làmpada",
+
+            "lâmpáda"
+
+        ]
+
+    },
+
+    {
+
+        numb: 33,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'manha':",
+
+        answer: "manhã",
+
+        options: [
+
+            "manhã",
+
+            "manha",
+
+            "máñha",
+
+            "mánhâ"
+
+        ]
+
+    },
+
+    {
+
+        numb: 34,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'ninguem':",
+
+        answer: "ninguém",
+
+        options: [
+
+            "ninguem",
+
+            "nínguem",
+
+            "ningúem",
+
+            "níngúem"
+
+        ]
+
+    },
+
+    {
+
+        numb: 35,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'oxigenio':",
+
+        answer: "oxigênio",
+
+        options: [
+
+            "oxigenio",
+
+            "oxigênio",
+
+            "oxigéñio",
+
+            "oxigêniô"
+
+        ]
+
+    },
+
+    {
+
+        numb: 36,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'rubrica':",
+
+        answer: "rúbrica",
+
+        options: [
+
+            "rúbrica",
+
+            "rubríca",
+
+            "rùbrica",
+
+            "rúbriça"
+
+        ]
+
+    },
+
+    {
+
+        numb: 37,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'urgente':",
+
+        answer: "urgente",
+
+        options: [
+
+            "urgente",
+
+            "úrgente",
+
+            "urgénte",
+
+            "urgênte"
+
+        ]
+
+    },
+
+    {
+
+        numb: 38,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'ciume':",
+
+        answer: "ciúme",
+
+        options: [
+
+            "ciúme",
+
+            "ciume",
+
+            "ciùme",
+
+            "cíume"
+
+        ]
+
+    },
+
+    {
+
+        numb: 39,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'espirito':",
+
+        answer: "espírito",
+
+        options: [
+
+            "espírito",
+
+            "espiríto",
+
+            "espirito",
+
+            "espíríto"
+
+        ]
+
+    },
+
+    {
+
+        numb: 40,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'ideia':",
+
+        answer: "ideia",
+
+        options: [
+
+            "idéia",
+
+            "ideia",
+
+            "idêia",
+
+            "idèia"
+
+        ]
+
+    },
+
+    {
+
+        numb: 41,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'juridico':",
+
+        answer: "jurídico",
+
+        options: [
+
+            "jurídico",
+
+            "juridico",
+
+            "jurídíco",
+
+            "jurídìco"
+
+        ]
+
+    },
+
+    {
+
+        numb: 42,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'lapis':",
+
+        answer: "lápis",
+
+        options: [
+
+            "lápis",
+
+            "lapis",
+
+            "lápís",
+
+            "lápîs"
+
+        ]
+
+    },
+
+    {
+
+        numb: 43,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'quimica':",
+
+        answer: "química",
+
+        options: [
+
+            "química",
+
+            "quimíca",
+
+            "químíca",
+
+            "quimica"
+
+        ]
+
+    },
+
+    {
+
+        numb: 44,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'rigido':",
+
+        answer: "rígido",
+
+        options: [
+
+            "rígido",
+
+            "rígiđo",
+
+            "rìgido",
+
+            "ríğido"
+
+        ]
+
+    },
+
+    {
+
+        numb: 45,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'tecnico':",
+
+        answer: "técnico",
+
+        options: [
+
+            "técníco",
+
+            "tecniço",
+
+            "técnico",
+
+            "técniço"
+
+        ]
+
+    },
+
+    {
+
+        numb: 46,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'util':",
+
+        answer: "útil",
+
+        options: [
+
+            "utíl",
+
+            "útil",
+
+            "útíl",
+
+            "ùtíl"
+
+        ]
+
+    },
+
+    {
+
+        numb: 47,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'veneno':",
+
+        answer: "veneno",
+
+        options: [
+
+            "veneno",
+
+            "vèneno",
+
+            "véneno",
+
+            "vêneno"
+
+        ]
+
+    },
+
+    {
+
+        numb: 48,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'zangado':",
+
+        answer: "zangado",
+
+        options: [
+
+            "zangado",
+
+            "zàngado",
+
+            "zângado",
+
+            "zàngâdo"
+
+        ]
+
+    },
+
+    {
+
+        numb: 49,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'cenoura':",
+
+        answer: "cenoura",
+
+        options: [
+
+            "cenôura",
+
+            "cenoura",
+
+            "cènoura",
+
+            "cênoura"
+
+        ]
+
+    },
+
+    {
+
+        numb: 50,
+
+        question: "Marque o local onde deve ser colocado o acento na palavra 'historico':",
+
+        answer: "histórico",
+
+        options: [
+
+            "historico",
+
+            "histórico",
+
+            "históríco",
+
+            "histôrico"
+
+        ]
+
+    }
+
+];
